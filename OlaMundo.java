c5d37f86d3388447e4bca5c35b2ae4ba763c3a2e
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-07-06 20:41:31 -0300

    first commit

---
+++ OlaMundo.java
@@ -0,0 +1,18 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.print("Ola Mundo, meu primeiro programa em Java");
+    }
+}
+
+// metodo Principal, Primeira Class que vai ser executada no Arquivo
+
+// é um metado Público (public)
+// um método estatico (static) é um metodo que pode ser acessado sem que eu
+// precise instanciar um objeto dessa class
+
+// void indica o Retorno do meu método , o tipo de dados que esse método retorna
+// (o tipo de dado void não retorna nada) mas se adicionar o int (ele vai
+// retornar um valor inteiro para quem fez a chamada)
+
+// main método principal, que tem um parametro de entrada, que é um array de
+// strings chamado "args"
